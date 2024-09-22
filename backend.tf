c9d96e88290873ae346988ba2ab4b0a89bc8e0e6
Harikrishna G R <hari.krishna@redingtongroup.com>
2024-09-22 21:38:09 +0530

    newewww23131yte

--- backend.tf
+++ backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket = "do-not-delete-myhkbucket"        #replace with the project specific bucketname
     key    = "backend/terraform.tfstate"
-    region = "ap-south-1"                                          #replce the region
-    dynamodb_table = "terraform-state-lock-DO-NOT-DELETE"          #do not change the dynamoDB table name
+    region = "ap-south-1"                                         
   }
 }
