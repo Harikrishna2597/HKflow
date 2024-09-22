11658036ad294d7cc7e1c4917a18062e9cba8802
Harikrishna G R <hari.krishna@redingtongroup.com>
2024-09-22 23:58:22 +0530

    newewww23131yte

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_instance_new2" {
   ami           = "ami-0ad21ae1d0696ad58" 
   instance_type = "t2.micro"
   key_name      = aws_key_pair.newkeypair2.key_name
-  depends_on = [aws_s3_bucket.newbucket]
+  
 
   tags = {
     Name = "MyEC2Instance"
@@ -13,6 +13,7 @@ resource "aws_instance" "my_instance_new2" {
 resource "aws_key_pair" "newkeypair2" {
 key_name = var.hk12_key
 public_key = tls_private_key.rsa_hk_new2.public_key_openssh
+depends_on = [aws_s3_bucket.newbucket]
 }
 
 #key pair which has been created above is encrypted with RSA algorithm 4096 bits, we can change this accroding to requirement 
