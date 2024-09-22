d4db34627bc8e0fd37467169955a321eb3c4780f
Harikrishna G R <hari.krishna@redingtongroup.com>
2024-09-22 23:40:03 +0530

    newewww23131yte

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "my_instance_new1" {
+resource "aws_instance" "my_instance_new2" {
   ami           = "ami-0ad21ae1d0696ad58" 
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.newkeypair.key_name
+  key_name      = aws_key_pair.newkeypair2.key_name
 
   tags = {
     Name = "MyEC2Instance"
@@ -9,20 +9,20 @@ resource "aws_instance" "my_instance_new1" {
 }
 
 
-resource "aws_key_pair" "newkeypair" {
+resource "aws_key_pair" "newkeypair2" {
 key_name = var.hk12_key
-public_key = tls_private_key.rsa_hk_new.public_key_openssh
+public_key = tls_private_key.rsa_hk_new2.public_key_openssh
 }
 
 #key pair which has been created above is encrypted with RSA algorithm 4096 bits, we can change this accroding to requirement 
-resource "tls_private_key" "rsa_hk_new" {
+resource "tls_private_key" "rsa_hk_new2" {
 algorithm = "RSA"
 rsa_bits  = 4096
 }
 
 #this creates a file in our working directory and download the aws key pair which we have created above
-resource "local_file" "hk_key_new" {
-content  = tls_private_key.rsa_hk_new.private_key_pem
+resource "local_file" "hk_key_new2" {
+content  = tls_private_key.rsa_hk_new2.private_key_pem
 filename = var.hk12_key
 depends_on = [aws_key_pair.newkeypair]
 }
