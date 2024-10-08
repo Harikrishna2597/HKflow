bfbff12da9590a5551633a6cff4fd2d029d32f6a
Harikrishna2597 <139646362+Harikrishna2597@users.noreply.github.com>
2024-10-08 12:58:59 +0530

    dsyresyres

--- variable.tf
+++ variable.tf
@@ -3,8 +3,3 @@ description = "hknv2325"
 default = "hknv2325"  
 }
 
-variable "hk12_key" {
-  description = "hknewkwy123"
-  type = string
-  default = "hknewkey.pem"   #create keypair with required name
-}
