e0fe9781096a1a8e04e1708326e27e1f24a9bbc6
Harikrishna2597 <hkrgtn@gmail.com>
2024-10-11 19:42:10 +0530

    checkwhy

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "do-not-delete-myhkbucket"        #replace with the project specific bucketname
-    key    = "backend/terraform.tfstate"
-    region = "ap-south-1"                                         
-  }
-}
--- s3.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "newbucket" {
-    bucket = var.s3
-}
